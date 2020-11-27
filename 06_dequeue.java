0ef104ebd333ea595aa31e27c42eea67d726f820
cupertinii <aditi.cupertino@outlook.com>
2020-11-26 17:45:01 -0800

    Updated more methods to dq

--- 06_dequeue.java
+++ 06_dequeue.java
@@ -3,7 +3,7 @@ import java.util.*;
 // double ended queue
 // Pronounced as "Deck"
 // Allow insertion and removal from both the ends
-// 
+//
 class MyCollections {
   public static void main(String args[]) {
     example_queue();
@@ -23,11 +23,25 @@ class MyCollections {
     // Size of the dequeue
     System.out.println("\nWorkday DQ Size : " + workingDaysInDeque.size() );
 
-    // In a dequeue, elements are added at the end
+    // In a dequeue, elements are added at the end using regular add method
     workingDaysInDeque.add("Saturday");
+
+    // In a dequeue, elements are added at the start using addFirst method
+    workingDaysInDeque.addFirst("xxx");
+
+    // In a dequeue, elements are added at the end using addLast method = add method
+    workingDaysInDeque.addLast("Sunday");
 
     // In a dequeue, elements are removed from the start
     String item = workingDaysInDeque.remove();
+    System.out.println("Removed item {" + item + "} from the DQ");
+
+    // In a dequeue, elements are removed from the start using poll
+    item = workingDaysInDeque.poll();
+    System.out.println("Removed item {" + item + "} from the DQ");
+
+    // In a dequeue, elements are removed from the start using pollLast
+    item = workingDaysInDeque.pollLast();
     System.out.println("Removed item {" + item + "} from the DQ");
 
     // Accessing elements in a queue
